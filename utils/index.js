22bfedead25772c0208450335929d2df19260d61
zhengjialux <15879398507@163.com>
2023-09-09 17:53:27 +0800

    修改项目结构

---
+++ utils/index.js
@@ -0,0 +1,18 @@
+// 生成图表 x 轴
+export const axis_x = (svgDom, scale) => {
+  console.log(scale.domain())
+  // 轴内容的最外层盒子
+  // const xAxisEl = svgDom.append('g')
+  //   .attr('font-size', 10)
+  //   .attr('text-anchor', 'middle')
+
+  // // 轴具体内容
+  // xAxisEl.append('path')
+  //   .attr('stroke', 'black')
+  //   .attr('d', '')
+}
+
+// 生成图表 y 轴
+export const axis_y = () => {
+
+}
